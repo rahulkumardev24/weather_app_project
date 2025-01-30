5e15ec63811d0f67e2ffad383b5bb1d7a83fee13
rahul <rkrahulroy151617@gmail.com>
2025-01-31 01:13:19 +0530

    create and alert message screen

---
+++ lib/screen/alert_message_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AlertMessageScreen extends StatefulWidget {
+  const AlertMessageScreen({super.key});
+
+  @override
+  State<AlertMessageScreen> createState() => _AlertMessageScreenState();
+}
+
+class _AlertMessageScreenState extends State<AlertMessageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
